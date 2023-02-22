c496ef8cd4c921e5afde041b69254be3696322c5
watashi407 <hyperteddy70@gmail.com>
2023-02-23 00:24:43 +0800

    new update

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
 
 
  
-    <div  className="bg-[#f7f7f6] container w-[1200px] h-[650px]  mx-auto rounded-[32px] shadow-inner ... drop-shadow-2xl flex">
+    <div  className="bg-[#f7f7f6] container w-[1000px] h-[500px]  mx-auto rounded-[32px] shadow-inner ... drop-shadow-2xl flex">
     
-      <div className="w-1/2 p-5 hidden md:flex rounded-[32px] bg-gradient-to-r  via-[#e5d8ec] from-[#ecadfff8] ...">
+      <div className="w-1/2 p-5 hidden md:flex rounded-[32px] bg-gradient-to-r  via-[#e6daed] from-[#cc77e6f8] ...">
 
-        <div className="flex justify-center">
-        <img className="mt-12 object-center w-70 h-70 ..." src={Image}  alt={Image}/>
+        <div className="justify-center">
+        <img className="mt-12 object-center w-30 h-30 ..." src={Image}  alt={Image}/>
         </div>
 
           
@@ -23,7 +23,7 @@ function App() {
       <div className="w-1/2 p-3 sm:mx-auto">
 
       <div className="flex relative justify-center items-center ">
-      <h1 className=" font-sans text-lg font-bold mt-[80px] flex pr-[320px] mb-2 animate-pulse">GET IN TOUCH</h1>
+      <h1 className=" font-sans text-lg font-bold mt-[20px] flex pr-[320px] mb-2 animate-pulse">GET IN TOUCH</h1>
       </div>
  
       <div className="flex justify-center sm:mx-auto ">
